b3b07d9509fede3ef9b77965f8aac16391e64f4f
Minhaz <minhaz3896@gmail.com>
2023-09-12 18:32:33 +0600

    commit 2

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -3,19 +3,45 @@ import logo from '../assets/logo.png'
 
 const Partners = () => {
   return (
-    <div name='partners'>
+    <div name='partners' className='w-full h-screen'>
+
         {/* Container */}
-        <div>
-            <div>
-            <p> Our Partners</p>
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+            <div className='pr-3 '>
+              <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-600 '> Partners</p>
             </div>
 
-            <div>
-                <img src={logo} alt='icon'/>
+          {/*Icon container  */}
+            <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+              <div className=' shadow-md shadow-[grey] hover:scale-110 duration-500 '>
+                  <img className='w-20 mx-auto' src={logo} alt='icon'/>
+                  <p className='my-4'>Partner</p>
+              </div>
+              <div className=' shadow-md shadow-[grey] hover:scale-110 duration-500 '>
+                  <img className='w-20 mx-auto' src={logo} alt='icon'/>
+                  <p className='my-4'>Partner</p>
+              </div>
+              <div className=' shadow-md shadow-[grey] hover:scale-110 duration-500 '>
+                  <img className='w-20 mx-auto' src={logo} alt='icon'/>
+                  <p className='my-4'>Partner</p>
+              </div>
+              <div className=' shadow-md shadow-[grey] hover:scale-110 duration-500 '>
+                  <img className='w-20 mx-auto' src={logo} alt='icon'/>
+                  <p className='my-4'>Partner</p>
+              </div>
+              <div className=' shadow-md shadow-[grey] hover:scale-110 duration-500 '>
+                  <img className='w-20 mx-auto' src={logo} alt='icon'/>
+                  <p className='my-4'>Partner</p>
+              </div>
+              <div className=' shadow-md shadow-[grey] hover:scale-110 duration-500 '>
+                  <img className='w-20 mx-auto' src={logo} alt='icon'/>
+                  <p className='my-4'>Partner</p>
+              </div>
+
             </div>
         </div>
     </div>
-  )
-}
+  );
+};
 
-export default Partners
+export default Partners;
